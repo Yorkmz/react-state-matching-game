84320b841cae5358eab30c81704813670fbef306
York Liu <york_liu@hotmail.com>
2022-07-02 16:48:46 +1000

    Completed M2 All errors are fixed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -31,7 +31,7 @@ class App extends Component {
         <header className="App-header">
           Turbo-Matcher
         </header>
-        <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.state.startGame} />
+        <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame} />
         <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
       </div>
     );
