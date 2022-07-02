e082ea635ea19ca9b5a4a4a476daf742f3fa15e9
York Liu <york_liu@hotmail.com>
2022-07-02 15:44:14 +1000

    M2 S9 second attempt

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './Tile.css'
 
 const Tile = (props) => {
-  const dynamicColor = props.seleted || props.matched ? { backgroundColor: color } : { backgroundColor: null }
+  const dynamicColor = props.seleted || props.matched ? { backgroundColor: props.color } : null
   return (
     <div className='Tile' style={dynamicColor}>
     </div>
